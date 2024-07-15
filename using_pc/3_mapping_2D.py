7e61fdba72d738ed3c3fc152260697af264b86fe
표승현 <42665051+PyoSH@users.noreply.github.com>
2024-07-15 10:55:25 +0900

    Add files via upload

---
+++ using_pc/3_mapping_2D.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 13:49:17 2024
+
+@author: pyo
+"""
+
+from data_processing import *
+import os 
+from copy import deepcopy
+
+dataset_path = '/home/pyo/b1_perception/exercise_mapping'
+pcd_path = os.path.join(dataset_path, 'accumulated.pcd')
+
+if __name__ =='__main__':
+    
+    ### 1. load accumulated pcd
+    pcd = o3d.io.read_point_cloud(pcd_path)
+    
+    ### 2.data denoise and downsampling
+    
+    ## 2.1 denoise
+    cl, idx = pcd.remove_statistical_outlier(nb_neighbors=20, std_ratio=2.0)
+    denoised_pcd = pcd.select_by_index(idx)
+    
+    ## 2.2 downsampling
+    cp_pcd = deepcopy(denoised_pcd)
+    down_pcd = cp_pcd.voxel_down_sample(voxel_size=0.05)
+    
+    ## 2.3 crop Z
+    np_pcd = np.array(cp_pcd.points)
+    croped_np_pcd = np_pcd[(np_pcd[:,2] >= 0.2) & (np_pcd[:,2] <= 0.6)]
+
+    croped_pcd = o3d.geometry.Pointcloud()
+    croped_pcd.points = o3d.utility.Vector3dVector(croped_np_pcd)
+
+    o3d.visualization.draw_geometries([croped_pcd])
+    
+    
